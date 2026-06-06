0738c90c01fa6d326811b8d03ae9a25ea137caca
Narendhiran15 <narendhiran_v@live.in>
2026-06-07 01:50:55 +0530

    Schema validation part 1

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -1,8 +1,9 @@
 import { test } from '../utils/fixtures'
 import { RequestHandler } from '../utils/request-handler'
 import { expect } from '@playwright/test'
+import { validateSchema } from '../utils/schema-validator'
 
-let authToken:string
+let authToken: string
 
 test.beforeAll('Get Authorization token', async ({ api }) => {
     const tokenResponse = await api
@@ -38,7 +39,7 @@ test('test case 2 : Get test tags', async ({ api }) => {
     const response = await api
         .path('tags')
         .getRequest(200)
-    console.log(response)
+    await validateSchema('tags', 'GET_tags',response)
 
     expect(response.tags[8]).toEqual('GitHub')
     expect(response.tags.length).toBeLessThanOrEqual(10)
@@ -48,17 +49,17 @@ test('test case 2 : Get test tags', async ({ api }) => {
 test('Create and delete Article', async ({ api }) => {
     const createArticleResponse = await api
         .path('articles')
-        .body({"article": {"title": "create new article_June02","description": "Desinged Framework", "body": "sddfsf", "tagList": [ "sdfdfs"]}})
-        .header({Authorization:authToken})
+        .body({ "article": { "title": "create new article_June02", "description": "Desinged Framework", "body": "sddfsf", "tagList": ["sdfdfs"] } })
+        .header({ Authorization: authToken })
         .postRequest(201)
-        expect(createArticleResponse.article.title).toEqual('create new article_June02')
-        const slugID = createArticleResponse.article.slug
+    expect(createArticleResponse.article.title).toEqual('create new article_June02')
+    const slugID = createArticleResponse.article.slug
 
     const getArticleResponse = await api
-    
+
         .path('/articles')
-        .params({limit:10,offset:0})
-        .header({Authorization:authToken})
+        .params({ limit: 10, offset: 0 })
+        .header({ Authorization: authToken })
         .getRequest(200)
 
     expect(getArticleResponse.articles[0].title).toEqual('create new article_June02')
@@ -67,71 +68,71 @@ test('Create and delete Article', async ({ api }) => {
     const deleteArticleResponse = await api
 
         .path(`articles/${slugID}`)
-        .header({Authorization:authToken})
+        .header({ Authorization: authToken })
         .deleteRequest(204)
 
     const getArticleResponseTwo = await api
-    
+
         .path('/articles')
-        .params({limit:10,offset:0})
-        .header({Authorization:authToken})
+        .params({ limit: 10, offset: 0 })
+        .header({ Authorization: authToken })
         .getRequest(200)
 
-    expect(getArticleResponseTwo.articles[0].title).not.toEqual('create new article_June02')    
+    expect(getArticleResponseTwo.articles[0].title).not.toEqual('create new article_June02')
 
 })
 
-test('create update and delete request',async({api})=>{
+test('create update and delete request', async ({ api }) => {
 
     const createArticleResponse = await api
         .path('articles')
-        .body({"article": {"title": "create new article_June02","description": "Desinged Framework", "body": "sddfsf", "tagList": [ "sdfdfs"]}})
-        .header({Authorization:authToken})
+        .body({ "article": { "title": "create new article_June02", "description": "Desinged Framework", "body": "sddfsf", "tagList": ["sdfdfs"] } })
+        .header({ Authorization: authToken })
         .postRequest(201)
-        expect(createArticleResponse.article.title).toEqual('create new article_June02')
-        const slugID = createArticleResponse.article.slug
+    expect(createArticleResponse.article.title).toEqual('create new article_June02')
+    const slugID = createArticleResponse.article.slug
 
     const getArticleResponse = await api
-    
+
         .path('/articles')
-        .params({limit:10,offset:0})
-        .header({Authorization:authToken})
+        .params({ limit: 10, offset: 0 })
+        .header({ Authorization: authToken })
         .getRequest(200)
 
     expect(getArticleResponse.articles[0].title).toEqual('create new article_June02')
 
     const updateArticleResponse = await api
         .path(`articles/${slugID}`)
-        .header({Authorization:authToken})
-        .body({"article": {"title": "update new article_June02","description": "Desinged Framework", "body": "sddfsf", "tagList": [ "sdfdfs"]}})
+        .header({ Authorization: authToken })
+        .body({ "article": { "title": "update new article_June02", "description": "Desinged Framework", "body": "sddfsf", "tagList": ["sdfdfs"] } })
         .putRequest(200)
-        expect(updateArticleResponse.article.title).toEqual('update new article_June02')
-        const update_slugID =updateArticleResponse.article.slug
-        
+    expect(updateArticleResponse.article.title).toEqual('update new article_June02')
+    const update_slugID = updateArticleResponse.article.slug
+
 
     const getUpdateArticleResponse = await api
-    
+
         .path('/articles')
-        .params({limit:10,offset:0})
-        .header({Authorization:authToken})
+        .params({ limit: 10, offset: 0 })
+        .header({ Authorization: authToken })
         .getRequest(200)
 
-    expect(getUpdateArticleResponse.articles[0].title).toEqual('update new article_June02')  
-    
+    expect(getUpdateArticleResponse.articles[0].title).toEqual('update new article_June02')
+
     const deleteArticleResponse = await api
 
         .path(`articles/${update_slugID}`)
-        .header({Authorization:authToken})
+        .header({ Authorization: authToken })
         .deleteRequest(204)
 
     const getArticleResponseTwo = await api
-    
+
         .path('/articles')
-        .params({limit:10,offset:0})
-        .header({Authorization:authToken})
+        .params({ limit: 10, offset: 0 })
+        .header({ Authorization: authToken })
         .getRequest(200)
 
-    expect(getArticleResponseTwo.articles[0].title).not.toEqual('update new article_June02') 
+    expect(getArticleResponseTwo.articles[0].title).not.toEqual('update new article_June02')
 })
 
 
