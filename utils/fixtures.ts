4c2b8c1860387bb3aad96b47147571d6626d28fc
Narendhiran15 <narendhiran_v@live.in>
2026-06-09 23:09:52 +0530

    Before custom-expect.ts file addition

--- utils/fixtures.ts
+++ utils/fixtures.ts
@@ -1,5 +1,6 @@
 import {test as base} from '@playwright/test'
 import { RequestHandler } from './request-handler'
+import { APIlogger } from './logger'
 
 export type Myfixtures ={
     api:RequestHandler
@@ -8,8 +9,8 @@ export type Myfixtures ={
 export const test = base.extend<Myfixtures>({
     api:async({request},use)=>{
         const defaultBaseUrl ='https://conduit-api.bondaracademy.com/api/'
-
-        const requestHandler =new RequestHandler(request,defaultBaseUrl)
+        const logger = new APIlogger()
+        const requestHandler =new RequestHandler(request,defaultBaseUrl,logger)
         await use(requestHandler)   
     }
 })
--- utils/request-handler.ts
+++ utils/request-handler.ts
@@ -1,8 +1,10 @@
 import { APIRequestContext, expect } from "@playwright/test"
+import { APIlogger } from "./logger"
 
 export class RequestHandler {
 
     private request: APIRequestContext
+    private logger: APIlogger
     private baseUrl: undefined | string = undefined
     private apiPath: string = ''
     private defaultBaseUrl: undefined | string
@@ -10,9 +12,10 @@ export class RequestHandler {
     private apiHeaders: Record<string, string> = {}
     private apibody: object = {}
 
-    constructor(request: APIRequestContext, apiBaseurl: string) {
+    constructor(request: APIRequestContext, apiBaseurl: string, logger: APIlogger) {
         this.request = request
         this.defaultBaseUrl = apiBaseurl
+        this.logger = logger
     }
 
 
@@ -49,46 +52,72 @@ export class RequestHandler {
         return url.toString()
     }
 
-    async getRequest(statusCode:number) {
+    async getRequest(statusCode: number) {
         const url = this.getUrl()
+        this.logger.logRequest('GET', url, this.apiHeaders, this.apibody)
         const response = await this.request.get(url, {
             headers: this.apiHeaders
         })
-        expect(response.status()).toEqual(statusCode)
-        const responseJson = await response.json()        
-        return responseJson
-       
-    }
 
-    async postRequest(statusCode:number){
-        const url = this.getUrl()
-        const response = await this.request.post(url,{
-            data:this.apibody,
-            headers:this.apiHeaders
-        })
-        expect(response.status()).toEqual(statusCode)
+        const actualStatus = response.status()
         const responseJson = await response.json()
+        this.logger.logResponse(statusCode, responseJson)
+        this.statusCodeValidator(actualStatus,statusCode,this.getRequest)
+
         return responseJson
+
     }
 
-    async putRequest(statusCode:number){
+    async postRequest(statusCode: number) {
         const url = this.getUrl()
-        const response = await this.request.put(url,{
-           data:this.apibody,
-           headers:this.apiHeaders 
+        this.logger.logRequest('POST', url, this.apiHeaders, this.apibody)
+        const response = await this.request.post(url, {
+            data: this.apibody,
+            headers: this.apiHeaders
         })
-        expect(response.status()).toEqual(statusCode)
+
+        const actualStatus = response.status()
         const responseJson = await response.json()
+        this.logger.logResponse(statusCode, responseJson)
+        this.statusCodeValidator(actualStatus,statusCode,this.postRequest)
         return responseJson
     }
 
-    async deleteRequest(statusCode:number){
+    async putRequest(statusCode: number) {
         const url = this.getUrl()
-        const response = await this.request.delete(url,{
-            headers:this.apiHeaders
+        this.logger.logRequest('PUT', url, this.apiHeaders, this.apibody)
+        const response = await this.request.put(url, {
+            data: this.apibody,
+            headers: this.apiHeaders
         })
-        expect(response.status()).toEqual(statusCode)
 
+        const actualStatus = response.status()
+        const responseJson = await response.json()
+        this.logger.logResponse(statusCode, responseJson)
+        this.statusCodeValidator(actualStatus,statusCode,this.putRequest)
+        return responseJson
+    }
+
+    async deleteRequest(statusCode: number) {
+        const url = this.getUrl()
+        this.logger.logRequest('DELETE', url, this.apiHeaders)
+        const response = await this.request.delete(url, {
+            headers: this.apiHeaders
+        })
+        const actualStatus = response.status()
+        this.logger.logResponse(statusCode)
+        this.statusCodeValidator(actualStatus,statusCode,this.deleteRequest)
+
+    }
+
+    private statusCodeValidator(actualStatus: number, expectStatus: number,callingFunction:Function) {
+        if (actualStatus !== expectStatus) {
+            const logs = this.logger.getRecentlogs()
+            const error = new Error(`Expected status is ${expectStatus} but we got ${actualStatus}\n\n
+                RecentAPI Activity: \n${logs}`)
+            Error.captureStackTrace(error,callingFunction)
+            throw error    
+        }
     }
 
 
